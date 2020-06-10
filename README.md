a8df7972da80ac7c49fb159f6d206a35272a908f
Gabriel Fresatto <gabrielfresatto@gmail.com>
2020-06-10 14:56:25 -0300

    Format price, waitForElement

--- README.md
+++ README.md
@@ -12,5 +12,10 @@ Como usar?
 const { formatCep } = require("juni-utils");
 ```
 
-- formatCep(value):
+- formatCep(value: number | string):
   Essa função irá formatar um CEP
+
+- formatPrice(value: number)
+  Formata o valor inserido para moeda. Ex: 1000 => R$ 1000,00, 3937.09 => R$ 3937,09
+
+- waitForElement(func => Element): func(elementFound)
--- index.js
+++ index.js
@@ -5,6 +5,37 @@ function formatCep(value) {
     .replace(/(\d{5})(\d)/, "$1-$2");
 }
 
+const { format: formatPrice } = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+function waitForElement(func, disappear = false, timeout = 100) {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      // Exibe um console com o elemento que está sendo aguardado
+      // console.log("Searching...", func);
+      const el = func();
+
+      //   Caso esteja aguardando o elemento desaparecer
+      if (disappear) {
+        if (!el) {
+          resolve();
+          clearInterval(interval);
+        }
+      } else {
+        if (el) {
+          // Caso encontre o elemento resolve e passa o elemento pro then
+          resolve(el);
+          clearInterval(interval);
+        }
+      }
+    }, timeout);
+  });
+}
+
 module.exports = {
   formatCep,
+  formatPrice,
+  waitForElement,
 };
